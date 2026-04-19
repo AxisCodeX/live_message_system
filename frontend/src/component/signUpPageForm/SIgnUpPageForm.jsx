3b84b12d5bbe57a0486430eae274c68f78360ce3
Sushant <bsushant0006@gmail.com>
2026-04-19 12:27:59 +0545

    signup page created

--- frontend/src/component/signUpPageForm/SIgnUpPageForm.jsx
+++ frontend/src/component/signUpPageForm/SIgnUpPageForm.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import Input from "../Input/Input"
+import axios from 'axios'
+import config from "../../configs/config"
+import { useNavigate  } from 'react-router-dom'
 function SIgnUpPageForm() {
-  return (
-    <div>
-        <div className='headings'></div>
-        <div className='from'>
+    const {register , handleSubmit} = useForm()
+    const navigate = useNavigate()
+    const onSubmit = async (data)=>{
+      console.log("sending info to backend.");
+      
+      console.log (data)
+      const res = await axios.post(`${config.backend_uri}/sign-up`,{
+        email : data.email,
+        username : data.username,
+        password : data.password
+      },{
+        withCredentials:true
+      })
+      console.log(res);
 
+      if (res.status == 200){
+        navigate("/sign-in")
+      }
+      
+    }
+  return (
+    <div className='flex flex-col min-h-screen items-center justify-center'>
+        <div className='headings'></div>
+        <div className='from flex justify-center items-center'>
+          <form onSubmit={handleSubmit(onSubmit)}>
+
+            <div>
+              <Input type="email" label={"Email"} labelVis={true} placeholder="Enter your email." name="email" {...register("email",{required:true})}/>
+              <Input label={"Username"} labelVis={true} placeholder="Enter a username." name="username" {...register("username",{required:true,max:12})}/>
+              <Input type={"password"} label={"Password"} labelVis={true} placeholder=" Enter a password." name="password" {...register("password",{required : true})}/>
+            </div>
+            <div className='flex justify-center'>
+              <button  className='bg-blue-600 px-2 py-2 rounded-md my-2 hover:bg-blue-700 active:bg-blue-500' type="submit">Create account</button>
+            </div>
+
+          </form>
         </div>
     </div>
   )
